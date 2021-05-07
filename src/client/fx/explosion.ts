9efc5dbec7ec4a2c6bc028215f2478d5c8b88dec
lego man <rajawilliams24@gmail.com>
2021-05-07 07:49:03 -0700

    Particle: Add explosion file Also add some basic explosions

---
+++ src/client/fx/explosion.ts
@@ -0,0 +1,32 @@
+import { Debris, Workspace } from '@rbxts/services'
+import { Particle } from './particle'
+
+/** The default Roblox explosion. Used for testing. */
+export class DefaultExplosion extends Particle {
+  play (position: Vector3): void {
+    const explosion = new Instance('Explosion')
+    explosion.DestroyJointRadiusPercent = 0
+    explosion.BlastPressure = 0
+    explosion.Position = position
+    explosion.Parent = Workspace
+    Debris.AddItem(explosion, 5)
+  }
+}
+
+/** A replication of the '07 explosion. */
+export class OldExplosion extends Particle {
+  play (position: Vector3): void {
+    const part = new Instance('Part')
+    part.Name = 'Explosion'
+    part.Size = new Vector3(12, 12, 12)
+    part.Material = Enum.Material.SmoothPlastic
+    part.Position = position
+    part.Color = new Color3(1, 0, 0)
+    part.Anchored = true
+    part.CanCollide = false
+    part.Parent = Workspace
+    const mesh = new Instance('SpecialMesh', part)
+    mesh.MeshType = Enum.MeshType.Sphere
+    Debris.AddItem(part, 0.5)
+  }
+}
--- src/client/ui/stories/particles.story.tsx
+++ src/client/ui/stories/particles.story.tsx
@@ -1,4 +1,43 @@
+import Roact from '@rbxts/roact'
+import { DefaultExplosion, OldExplosion } from 'client/fx/explosion'
+import { Particle, ParticleOptions } from 'client/fx/particle'
+
+const particles: Map<string, new(options?: ParticleOptions) => Particle> = new Map()
+particles.set('explosion', DefaultExplosion)
+particles.set('07 explosion', OldExplosion)
+
+function buttonEvent (Class: new(options?: ParticleOptions) => Particle): () => void {
+  return () => {
+    new Class().play(new Vector3(0, 10, 0))
+  }
+}
+
+function getList (): Roact.Element[] {
+  const out: Roact.Element[] = []
+  particles.forEach((value, key) => {
+    out.push(<textbutton Event={{ Activated: buttonEvent(value) }} Text={key} BackgroundColor3={new Color3(1, 1, 1)} />)
+  })
+  return out
+}
+
+function ParticleTest (): Roact.Element {
+  return (
+    <frame
+      AnchorPoint={new Vector2(0.5, 0.5)}
+      Position={UDim2.fromScale(0.5, 0.5)}
+      Size={UDim2.fromScale(0.5, 0.5)}
+      SizeConstraint={Enum.SizeConstraint.RelativeYY}
+    >
+      <uigridlayout CellSize={new UDim2(1, 0, 0, 30)} />
+      {
+        getList()
+      }
+    </frame>
+  )
+}
 
 export = (target: Instance) => {
+  const tree = Roact.mount(<ParticleTest />, target)
 
+  return () => Roact.unmount(tree)
 }
