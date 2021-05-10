f7170c5e5441ebdb97960f261a27a1dadd7df432
lego man <rajawilliams24@gmail.com>
2021-05-10 16:59:40 -0700

    Projectiles: Predict path projectile prowls!
    Predict where projectiles will go, and fire relavent events that apply

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,7 +1,22 @@
+import { ipcClient } from '@rbxts/abstractify'
+import { Players } from '@rbxts/services'
+import { ProjectileMap, firePredicted } from 'shared/hitreg/projectile'
 import { promiseError } from 'shared/utils'
+import * as SlaveClock from 'shared/hitreg/clock'
 import mount from './ui'
 
 mount()
   .catch(promiseError)
+
+ipcClient.on('projectileFired', (time: number, origin: Vector3, direction: Vector3, player: Player, foundKey: string, ignore?: Instance) => {
+  if (player === Players.LocalPlayer) {
+    return
+  }
+  const projectile = ProjectileMap.get(foundKey)
+  if (projectile !== undefined) {
+    const timeAhead = SlaveClock.getTime() - time
+    firePredicted(timeAhead, origin, direction, projectile, ignore)
+  }
+}).catch(promiseError)
 
 // TODO: !! IMPORTANT !! find feasible place to put the time syncing.
--- src/shared/hitreg/projectile.ts
+++ src/shared/hitreg/projectile.ts
@@ -1,9 +1,9 @@
-import { ipcClient, ipcServer } from '@rbxts/abstractify'
+import { ipcServer } from '@rbxts/abstractify'
 import FastCast from '@rbxts/fastcast'
 import Object from '@rbxts/object-utils'
-import { Players, Workspace } from '@rbxts/services'
+import { Workspace } from '@rbxts/services'
 import { $dbg, $print } from 'rbxts-transform-debug'
-import { isClient, isServer, promiseError } from 'shared/utils'
+import { isClient, isServer } from 'shared/utils'
 import { PartialDeep } from './regtils'
 
 const _tmpProjFolder = Workspace.FindFirstChild('ProjectileFolder')
@@ -28,7 +28,7 @@ const defaultBehavior = FastCast.newBehavior()
 defaultBehavior.RaycastParams = new RaycastParams();
 (defaultBehavior as unknown as { CosmeticBulletContainer: Instance }).CosmeticBulletContainer = ProjectileFolder
 
-const defaultOptions: Projectile = {
+export const defaultOptions: Projectile = {
   behavior: defaultBehavior,
   velocity: 100,
   hit: () => {},
@@ -52,6 +52,7 @@ export function add (name: string, projectile: PartialDeep<Projectile>): Partial
 if (isServer()) {
   ipcServer.getEvent('projectileFired')
 }
+
 function addIgnore (projectile: PartialDeep<Projectile>, ignore: Instance): PartialDeep<Projectile> {
   // copy the projectile
   const out = Object.deepCopy(projectile)
@@ -82,8 +83,14 @@ export function fireReplicated (origin: Vector3, direction: Vector3, player: Pla
     }
   })
   if (foundKey !== undefined) {
-    ipcServer.broadcast('projectileFired', origin, direction, player, foundKey, ignore)
+    ipcServer.broadcast('projectileFired', tick(), origin, direction, player, foundKey, ignore)
   }
+}
+
+// TODO: make this follow a quadratic
+/** If the projectile follows a straight path, predict where it will be in timeAhead seconds. */
+export function predictPath (timeAhead: number, origin: Vector3, direction: Vector3, speed: number): Vector3 {
+  return origin.add(direction.Unit.mul(timeAhead * speed))
 }
 
 export function fire (origin: Vector3, direction: Vector3, dataPacket: PartialDeep<Projectile>, ignore?: Instance): void {
@@ -102,16 +109,75 @@ export function fire (origin: Vector3, direction: Vector3, dataPacket: PartialDe
   PROJECTILE_CASTER.Fire(origin, direction, currentOptions.velocity, currentOptions.behavior)
 }
 
-if (isClient()) {
-  ipcClient.on('projectileFired', (origin: Vector3, direction: Vector3, player: Player, foundKey: string, ignore?: Instance) => {
-    if (player === Players.LocalPlayer) {
-      return
+/** Predict where the projectile will be based on how long ago it was shot. Only applies to no acceleration projectiles. */
+export function firePredicted (
+  time: number,
+  origin: Vector3,
+  direction: Vector3,
+  dataPacket: PartialDeep<Projectile>,
+  ignore?: Instance
+): void {
+  const speed = dataPacket.velocity ?? defaultOptions.velocity
+  const predictedOrigin = predictPath(time, origin, direction, speed)
+  const predictedDirection = direction.Unit.mul(time * speed)
+  if (ignore !== undefined) {
+    dataPacket = addIgnore(dataPacket, ignore)
+  }
+  const cosmeticBulletContainer =
+    (dataPacket.behavior as unknown as { CosmeticBulletContainer?: Instance })?.CosmeticBulletContainer ??
+    ProjectileFolder
+  const params = dataPacket.behavior?.RaycastParams ?? new RaycastParams();
+  (params.FilterDescendantsInstances as defined[]).push(cosmeticBulletContainer)
+  if (ignore !== undefined) {
+    (params.FilterDescendantsInstances as defined[]).push(ignore)
+  }
+  const result = PROJECTILE_CASTER.WorldRoot.Raycast(origin, predictedDirection, params as RaycastParams)
+  if (result?.Instance !== undefined) {
+    $print('test')
+    // TODO: partcache joes
+    const cosemeticBulletTemplate = dataPacket.behavior?.CosmeticBulletTemplate as BasePart | undefined
+    // TODO: piercing
+    const lengthChanged = dataPacket.lengthChanged ?? defaultOptions.lengthChanged
+    const hit = dataPacket.hit ?? defaultOptions.hit
+    const terminated = dataPacket.terminated ?? defaultOptions.terminated
+    if (cosemeticBulletTemplate !== undefined) {
+      const newProjectile = cosemeticBulletTemplate.Clone()
+      newProjectile.Parent = cosmeticBulletContainer
+      const ActiveCastRemake = {
+        Caster: PROJECTILE_CASTER,
+
+        StateInfo: {
+          UpdateConnection: undefined as unknown as RBXScriptSignal,
+          Paused: false,
+          TotalRuntime: time,
+          DistanceCovered: time * speed,
+          IsActivelySimulatingPierce: false,
+          Trajectories: [
+            {
+              StartTime: 0,
+              EndTime: -1,
+              Origin: origin,
+              InitialVelocity: direction.mul(speed),
+              Acceleration: dataPacket.behavior?.Acceleration as Vector3 ?? new Vector3()
+            }
+          ]
+        },
+
+        RayInfo: {
+          Parameters: params as RaycastParams,
+          WorldRoot: PROJECTILE_CASTER.WorldRoot,
+          MaxDistance: dataPacket.behavior?.MaxDistance ?? 1000,
+          CosmeticBulletObject: newProjectile,
+          CanPierceCallback: dataPacket.behavior?.CanPierceFunction ?? (() => false)
+        },
+        UserData: {}
+      } as unknown as FastCast.ActiveCast
+      const distance = result.Position.sub(origin).Magnitude
+      lengthChanged(ActiveCastRemake, origin, direction, distance, predictedDirection, newProjectile)
+      hit(ActiveCastRemake, result, predictedDirection, newProjectile)
+      terminated(ActiveCastRemake)
     }
-    const projectile = ProjectileMap.get(foundKey)
-    if (projectile !== undefined) {
-      fire(origin, direction, projectile, ignore)
-    }
-  }).then((value) => {
-    $print(value)
-  }).catch(promiseError)
+  } else {
+    fire(predictedOrigin, direction, dataPacket) // we already did the ignore
+  }
 }
