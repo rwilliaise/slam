6c1c3b8d79095753e0a274e4fcb3ab0f43f0192a
lego man <rajawilliams24@gmail.com>
2021-05-12 19:09:20 -0700

    Meta: Actually add tests lol

---
+++ src/server/main.spec.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="@rbxts/testez/globals" />
+
+export = () => {
+  describe('testing', () => {
+    it('should work', () => {
+      expect(true).to.equal(true)
+    })
+  })
+}
